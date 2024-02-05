ee0c2d4f06383ee06a6319b9604526df3439c5d8
David Katsir <david.katsir@gmail.com>
2024-02-05 20:49:48 +0200

    Commit 03

--- src/SeleniumMain.java
+++ src/SeleniumMain.java
@@ -9,7 +9,5 @@ public class SeleniumMain {
         System.out.println(driver.getTitle());
         System.out.println(driver.getCurrentUrl());
         driver.quit();
-        System.out.println("Done");
-
     }
 }
