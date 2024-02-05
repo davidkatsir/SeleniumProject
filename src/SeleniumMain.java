41377e7a62a62901d466ce875978fd7cc56c60c1
David Katsir <david.katsir@gmail.com>
2024-02-05 20:01:04 +0200

    Initial commit

---
+++ src/SeleniumMain.java
@@ -0,0 +1,13 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeleniumMain {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.ebay.com/");
+        System.out.println(driver.getTitle());
+        System.out.println(driver.getCurrentUrl());
+        driver.quit();
+    }
+}
