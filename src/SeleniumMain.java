0a87d95513b4edc34f669273f82155192594d910
David Katsir <david.katsir@gmail.com>
2024-02-06 19:28:40 +0200

    Commit Test 1

--- src/SeleniumMain.java
+++ src/SeleniumMain.java
@@ -9,6 +9,6 @@ public class SeleniumMain {
         System.out.println(driver.getTitle());
         System.out.println(driver.getCurrentUrl());
         driver.quit();
-        System.out.println("Test");
+        System.out.println("Test1");
     }
 }
