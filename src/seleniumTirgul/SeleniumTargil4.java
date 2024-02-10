0c5c97607e932f36ab2bd55e252a4d3aea9812a8
David Katsir <david.katsir@gmail.com>
2024-02-10 14:47:32 +0200

    Commit 10-02-2024 01

--- src/seleniumTirgul/SeleniumTargil4.java
+++ src/seleniumTirgul/SeleniumTargil4.java
@@ -1,17 +1,77 @@
 package seleniumTirgul;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.Random;
+
 public class SeleniumTargil4 {
+
     public static void main(String[] args) throws InterruptedException {
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
-        driver.get("https://login.salesforce.com/");
 
+        // כאן אנחנו עולים רמה וכותבים את כל הלוקייטורים והמשתנים במרוכז למעלה
+        String salesforceURL = "https://login.salesforce.com/";
+        String usernameFieldLocator = "[id=\"username\"]";
+        String passwordFieldLocator = "[id=\"password\"]";
+        String rememberMeCheckboxLocator = "[id=\"rememberUn\"]";
+        String loginButtonLocator = "[id=\"Login\"]";
+        String errorMessageLocator = "[id=\"error\"]";
+        String password = "12345";
 
+        // Generate random number for username
+        Random random = new Random();
+        int randomNumber = random.nextInt(10000);
+        String username = "david" + randomNumber;
 
-        Thread.sleep(1000);
+        // Set expected error message
+        String expectedErrorMessage = "Please check your username and password. If you still can't log in, contact your Salesforce administrator.";
+
+        // Open Salesforce login page
+        driver.get(salesforceURL);
+
+        // Enter username
+        //                       WebElement להסביר את השימוש ב -
+        WebElement usernameField = driver.findElement(By.cssSelector(usernameFieldLocator));
+        usernameField.sendKeys(username);
+
+        // Enter password
+        WebElement passwordField = driver.findElement(By.cssSelector(passwordFieldLocator));
+        passwordField.sendKeys(password);
+
+        // Check 'remember me' checkbox
+        WebElement rememberMeCheckbox = driver.findElement(By.cssSelector(rememberMeCheckboxLocator));
+        rememberMeCheckbox.click();
+
+        // Verify checkbox is checked and click Login
+        if (rememberMeCheckbox.isSelected()) {
+            System.out.println("Remember me checkbox is checked.");
+            WebElement loginButton = driver.findElement(By.cssSelector(loginButtonLocator));
+            loginButton.click();
+        } else {
+            System.out.println("Error: Remember me checkbox is not checked.");
+        }
+
+        // Wait for a fixed time before checking for error message (potentially unreliable)
+        // Problems with Tread.sleep:
+        // Unreliable timing, can lead to flaky tests.
+        // Potentially slows down tests if element loads quickly.
+        Thread.sleep(1000);  // Wait for 1 second
+        WebElement errorMessage = driver.findElement(By.cssSelector(errorMessageLocator));
+
+        // Print error message and verify it matches expected message
+        String actualErrorMessage = errorMessage.getText();
+        System.out.println("Error message: " + actualErrorMessage);
+        if (actualErrorMessage.equals(expectedErrorMessage)) {
+            System.out.println("Error message matches expected message.");
+        } else {
+            System.out.println("Error message does not match expected message.");
+        }
+
+        // Close browser
         driver.quit();
     }
 }
--- src/seleniumTirgul/SeleniumTargil5.java
+++ src/seleniumTirgul/SeleniumTargil5.java
@@ -1,17 +1,72 @@
 package seleniumTirgul;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 
 public class SeleniumTargil5 {
+
     public static void main(String[] args) throws InterruptedException {
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
-        driver.get("");
 
+        // Navigate to MyTinyTodo page
+        driver.get("https://www.mytinytodo.net/demo/#list/2");
 
+        // Navigate to the "To do" tab
+         WebElement toDoTab = driver.findElement(By.cssSelector("#list_1 > a > div.title-block > span.title"));
+         toDoTab.click();
+         Thread.sleep(500);
 
+        // Add tasks
+        String task1 = "Task D1";
+        String task2 = "Task D2";
+        WebElement newTaskField = driver.findElement(By.cssSelector("[id=\"task\"]"));
+        newTaskField.sendKeys(task1 + "\n"); // Add task1 and press enter
+        // Check if Clear field is needed
+        newTaskField.sendKeys(task2 + "\n"); // Add task2 and press enter
+
+        // Verify total tasks
+        String totalTasksText = driver.findElement(By.cssSelector("[id=\"taskview\"] [id=\"total\"]")).getText();
+        if (totalTasksText.equals("2")) {
+            System.out.println("Total tasks correct: " + totalTasksText);
+        } else {
+            System.out.println("Error: Expected 2 tasks, found " + totalTasksText);
+        }
+
+        // Wait for page to load (Optional)
+        Thread.sleep(2000);
+
+        // Delete first task
+        WebElement firstTaskCheckbox = driver.findElement(By.cssSelector("[id=\"taskrow_1\"] [type=\"checkbox\"]"));
+        firstTaskCheckbox.click(); // Select the first task
+
+        Actions actions = new Actions(driver);
+        WebElement kebabMenu = driver.findElement(By.cssSelector("[class=\"taskactionbtn\"]"));
+
+        // Wait for kebab menu to appear (Optional)
         Thread.sleep(1000);
+
+        actions.moveToElement(kebabMenu).perform(); // Hover over kebab menu
+
+        // Ensure you have a unique selector for the delete option within the kebab menu
+        WebElement deleteOption = driver.findElement(By.cssSelector("[id=\"cmenu_delete\"]"));
+        actions.moveToElement(deleteOption).click().perform(); // Click delete
+
+        // Confirm deletion
+        driver.findElement(By.cssSelector("[id=\"btnModalOk\"]")).click(); // Click OK
+
+        // Verify updated total tasks
+        totalTasksText = driver.findElement(By.cssSelector("[id=\"taskview\"] [id=\"total\"]")).getText();
+        if (totalTasksText.equals("1")) {
+            System.out.println("Total tasks updated correctly: " + totalTasksText);
+        } else {
+            System.out.println("Error: Expected 1 task, found " + totalTasksText);
+        }
+
+        // Close browser
         driver.quit();
     }
 }
