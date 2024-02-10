2b989f0075b9cd77edb30f8a508c987cb2a1e29f
David Katsir <david.katsir@gmail.com>
2024-02-10 23:28:53 +0200

    Commit 10-02-2024 - 2

--- src/seleniumTirgul/SeleniumTargil5.java
+++ src/seleniumTirgul/SeleniumTargil5.java
@@ -12,54 +12,64 @@ public class SeleniumTargil5 {
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
 
-        // Navigate to MyTinyTodo page
-        driver.get("https://www.mytinytodo.net/demo/#list/2");
-
-        // Navigate to the "To do" tab
-         WebElement toDoTab = driver.findElement(By.cssSelector("#list_1 > a > div.title-block > span.title"));
-         toDoTab.click();
-         Thread.sleep(500);
-
-        // Add tasks
+        String myTinyTodoUrl = "https://www.mytinytodo.net/demo/#list/2";
+        String toDoTabLocator = "#list_1 > a > div.title-block > span.title";
         String task1 = "Task D1";
         String task2 = "Task D2";
-        WebElement newTaskField = driver.findElement(By.cssSelector("[id=\"task\"]"));
-        newTaskField.sendKeys(task1 + "\n"); // Add task1 and press enter
-        // Check if Clear field is needed
-        newTaskField.sendKeys(task2 + "\n"); // Add task2 and press enter
+        String newTaskFieldLocator = "[id=\"task\"]";
+        String addTaskButtonLocator = "[id=\"newtask_submit\"]";
+        String totalTasksTextLocator = "[id=\"taskview\"] [id=\"total\"]";
+        String kebabMenuLocator = "div.task-block > div.task-actions > div:nth-child(1)";
+        String deleteOptionLocator = "[id=\"cmenu_delete\"]";
+        String okButtonLocator = "[id=\"btnModalOk\"]";
+
+        // Navigate to MyTinyTodo page
+        driver.get(myTinyTodoUrl);
+
+        // Navigate to the "To do" tab
+        WebElement toDoTab = driver.findElement(By.cssSelector(toDoTabLocator));
+        toDoTab.click();
+        Thread.sleep(2000);
+
+        // Add tasks
+        WebElement newTaskField = driver.findElement(By.cssSelector(newTaskFieldLocator));
+        newTaskField.sendKeys(task1); // Add task1
+        WebElement addTaskButton = driver.findElement(By.cssSelector(addTaskButtonLocator));
+        addTaskButton.click();
+        Thread.sleep(300);
+        newTaskField.sendKeys(task2); // Add task2
+        addTaskButton.click();
 
         // Verify total tasks
-        String totalTasksText = driver.findElement(By.cssSelector("[id=\"taskview\"] [id=\"total\"]")).getText();
+        Thread.sleep(1000);
+        String totalTasksText = driver.findElement(By.cssSelector(totalTasksTextLocator)).getText();
         if (totalTasksText.equals("2")) {
-            System.out.println("Total tasks correct: " + totalTasksText);
+            System.out.println("Total tasks is correct: " + totalTasksText);
         } else {
             System.out.println("Error: Expected 2 tasks, found " + totalTasksText);
         }
 
         // Wait for page to load (Optional)
-        Thread.sleep(2000);
-
-        // Delete first task
-        WebElement firstTaskCheckbox = driver.findElement(By.cssSelector("[id=\"taskrow_1\"] [type=\"checkbox\"]"));
-        firstTaskCheckbox.click(); // Select the first task
-
-        Actions actions = new Actions(driver);
-        WebElement kebabMenu = driver.findElement(By.cssSelector("[class=\"taskactionbtn\"]"));
-
-        // Wait for kebab menu to appear (Optional)
         Thread.sleep(1000);
 
-        actions.moveToElement(kebabMenu).perform(); // Hover over kebab menu
+        // Delete first task
+        Actions actions = new Actions(driver);
+        WebElement kebabMenu = driver.findElement(By.cssSelector(kebabMenuLocator));
 
-        // Ensure you have a unique selector for the delete option within the kebab menu
-        WebElement deleteOption = driver.findElement(By.cssSelector("[id=\"cmenu_delete\"]"));
+        // Hover over kebab menu and click on it
+        actions.moveToElement(kebabMenu).perform();
+        Thread.sleep(1000); // Wait for kebab menu to appear
+        actions.moveToElement(kebabMenu).click().perform();
+
+        WebElement deleteOption = driver.findElement(By.cssSelector(deleteOptionLocator));
         actions.moveToElement(deleteOption).click().perform(); // Click delete
 
         // Confirm deletion
-        driver.findElement(By.cssSelector("[id=\"btnModalOk\"]")).click(); // Click OK
+        WebElement okButton = driver.findElement(By.cssSelector(okButtonLocator));
+        okButton.click(); // Click OK
+        Thread.sleep(2000);
 
         // Verify updated total tasks
-        totalTasksText = driver.findElement(By.cssSelector("[id=\"taskview\"] [id=\"total\"]")).getText();
         if (totalTasksText.equals("1")) {
             System.out.println("Total tasks updated correctly: " + totalTasksText);
         } else {
